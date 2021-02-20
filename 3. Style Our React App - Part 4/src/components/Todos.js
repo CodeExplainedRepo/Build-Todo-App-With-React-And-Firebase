58d86273203bf5052539c9e01d193a35b558a890
CodeExplainedRepo <41912954+CodeExplainedRepo@users.noreply.github.com>
2021-02-20 21:22:37 +0100

    Add files via upload

---
+++ 3. Style Our React App - Part 4/src/components/Todos.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Todo from './Todo'
+import Next7Days from './Next7Days'
+
+function Todos(){
+
+    return (
+        <div className='Todos'>
+            <Todo />
+            <Next7Days />
+        </div>
+    )
+}
+
+export default Todos
