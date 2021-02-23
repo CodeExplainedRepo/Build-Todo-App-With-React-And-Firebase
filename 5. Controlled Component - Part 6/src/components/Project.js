e6a0f174c9f27b97b4619d3408eb1ab059964569
CodeExplainedRepo <41912954+CodeExplainedRepo@users.noreply.github.com>
2021-02-24 00:09:54 +0100

    Add files via upload

---
+++ 5. Controlled Component - Part 6/src/components/Project.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import RenameProject from './RenameProject'
+
+function Project(){
+
+    return (
+        <div className='Project'>
+            <RenameProject />
+        </div>
+    )
+}
+
+export default Project
