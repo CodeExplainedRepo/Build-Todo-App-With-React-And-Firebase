2d80b7a084bc848220b69666b8bb45d41beb3254
CodeExplainedRepo <41912954+CodeExplainedRepo@users.noreply.github.com>
2021-02-26 21:35:12 +0100

    Add files via upload

---
+++ 6. Array map in React - Part 7/src/components/Projects.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import AddNewProject from './AddNewProject'
+import Project from './Project'
+
+function Projects(){
+
+    return (
+        <div className='Projects'>
+            <AddNewProject />
+            <Project />
+        </div>
+    )
+}
+
+export default Projects
